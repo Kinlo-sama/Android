01d666e28819cc674e68f58997ec1803a43e2919
Kinlo-sama <leohola43@gmail.com>
2023-06-14 02:11:11 -0600

    Cap3

--- Picador/app/src/main/java/com/example/picador/MainActivity.kt
+++ Picador/app/src/main/java/com/example/picador/MainActivity.kt
@@ -5,43 +5,39 @@ import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity() : AppCompatActivity(), Parcelable {
     constructor(parcel: Parcel) : this() {
     }
-
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
-        val botonInc: Button = findViewById(R.id.BotonIncremente)
         rollButton.setOnClickListener{ rollDice() }
-        botonInc.setOnClickListener{ Cuenta() }
+
+
 
     }
     private fun rollDice(){
         val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
         Toast.makeText(this,"Boton presionado",
         Toast.LENGTH_SHORT).show()
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(drawableResource)
     }
-    private fun Cuenta(){
-        val resulText: TextView = findViewById(R.id.result_text)
-        if (resulText.text == "Hola mundo"){
-            resulText.text = "1"
-        }
-        else{
-            var resultInt = resulText.text.toString().toInt()
-            if(resultInt < 6){
-                ++resultInt
-                resulText.text = resultInt.toString()
-            }
-        }
-    }
+
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
 
